81519c81f36d11b86ae88f60299e426047710f66
Julia Chepasova <juliachepasova@hotmail.com>
2022-11-10 18:33:21 +0300

    add initial cards

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://unsplash.com/photos/kvQ7X_fDJ8U",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://unsplash.com/photos/e-loWO1C3Lw",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://unsplash.com/photos/u49bj3nOPD8",
+  },
+  {
+    name: "Latemar",
+    link: "https://unsplash.com/photos/pBgnT4KH8d4",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://unsplash.com/photos/R261qkc-nDE",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://unsplash.com/photos/DEez1BPJW2c",
+  },
+];
